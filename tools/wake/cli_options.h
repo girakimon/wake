fdcceee7384c07036bbeeeab3525d43a0275a1d7
KW Justin Leung <justin.leung@sifive.com>
2026-09-02 10:38:45 -0700

    Add Tunnel Vision remote triage

--- tools/wake/cli_options.h
+++ tools/wake/cli_options.h
@@ -73,6 +73,7 @@ struct CommandLineOptions {
   bool include_hidden;
   bool ui;
   bool tui;
+  bool tunnel_vision;
   bool mcp;
   bool otel;
   bool rm;
@@ -207,6 +208,7 @@ struct CommandLineOptions {
       {0, "include-hidden", GOPT_ARGUMENT_FORBIDDEN},
       {0, "ui", GOPT_ARGUMENT_FORBIDDEN},
       {0, "tui", GOPT_ARGUMENT_FORBIDDEN},
+      {0, "tunnel-vision", GOPT_ARGUMENT_FORBIDDEN},
       {0, "mcp", GOPT_ARGUMENT_FORBIDDEN},
       {0, "otel", GOPT_ARGUMENT_FORBIDDEN},
       {0, "ui-address", GOPT_ARGUMENT_REQUIRED},
@@ -263,6 +265,7 @@ struct CommandLineOptions {
     include_hidden = arg(options, "include-hidden")->count;
     ui = arg(options, "ui")->count;
     tui = arg(options, "tui")->count;
+    tunnel_vision = arg(options, "tunnel-vision")->count;
     mcp = arg(options, "mcp")->count;
     otel = arg(options, "otel")->count;
     rm = arg(options, "rm")->count;
